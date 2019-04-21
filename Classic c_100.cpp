106ee15fa01822a459ecaed7c856ce59208f7d0c
WEPEDO <WEPED@outlook.com>
2019-04-21 12:52:45 +0800

    CommitMyCode

--- Classic c_100.cpp
+++ Classic c_100.cpp
@@ -3,7 +3,7 @@
  * @LastEditors: WenPeidong
  * @Description: file content
  * @Date: 2019-04-18 13:49:41
- * @LastEditTime: 2019-04-18 14:33:03
+ * @LastEditTime: 2019-04-21 12:52:27
  */
 
 
@@ -75,9 +75,84 @@
 //         {
 //             x = sqrt(i + 100);
 //             y = sqrt(i + 168);
-//             if(x*x == i + 100&&y*y == 1 +168)
+//             if(x*x == i+100&&y*y ==1+168)
 //                 printf("\n%ld\n", i); 
 //         }
 //     system("pause");  
 //     return 0;      
 // }
+
+// 程序4
+
+// # include<stdio.h>
+// # include<stdlib.h>
+
+// int main()
+// {
+//   int day,month,year,sum,leap;
+//   printf("\nplease input year,month,day\n");
+//   scanf("%d,%d,%d",&year,&month,&day);
+//   switch(month)/*先计算某月以前月份的总天数*/
+// {
+//     case 1:sum=0;break;
+//     case 2:sum=31;break;
+//     case 3:sum=59;break;
+//     case 4:sum=90;break;
+//     case 5:sum=120;break;
+//     case 6:sum=151;break;
+//     case 7:sum=181;break;
+//     case 8:sum=212;break;
+//     case 9:sum=243;break;
+//     case 10:sum=273;break;
+//     case 11:sum=304;break;
+//     case 12:sum=334;break;
+// 	default:
+// 		printf("data error");
+// 		break;
+// 	}
+// 	sum = sum + day;									   /*再加上某天的天数*/
+// 	if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) /*判断是不是闰年*/
+// 		leap = 1;
+// 	else
+// 		leap = 0;
+// 	if (leap == 1 && month > 2) /*如果是闰年且月份大于 2,总天数应该加一天*/
+// 		sum++;
+// 	system("pause");
+//     return 0;
+// }
+
+
+// 程序5
+// # include<stdio.h>
+// # include <stdlib.h>
+
+// int main()
+// {
+// 	int x, y, z, i;
+// 	printf("请输入三个数:\n");
+// 	scanf("%d %d %d\n", &x,&y,&z);
+// 	if(x>y)
+// 		{i = x;x = y;y = i;}
+// 	if (x > z)
+// 		{i = z;z = x;x = i;}
+// 	if (y > z)
+// 		{i = y;y = z;z = i;}
+// 	printf("从小到大为：%d %d %d\n", x, y, z);
+// 	system("pause");
+// 	return 0;
+// }
+
+// 程序6
+// # include<stdio.h>
+// # include<stdlib.h>
+
+// main()
+// {
+// 	printf("Hello C-world!\n");
+// 	printf(" ****\n");
+// 	printf(" *\n");
+// 	printf(" * \n");
+// 	printf(" ****\n");
+// 	system("pause");
+// }
+
